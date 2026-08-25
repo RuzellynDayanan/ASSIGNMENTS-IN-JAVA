ea069162d286510165db5cb5e1c715993e3cd6b1
Ruzellyn C. Dayanan <ruzellyndayanan@gmail.com>
2026-08-25 22:01:25 +0800

    Add files via upload

---
+++ java/Display.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Display{
+	public static void main(String [] args){
+	Scanner sc = new Scanner(System.in);
+		int [] num = new int [n];
+		
+		System.out.print("Enter a number: ");
+		for(int i=0; i < n.length; i++){
+			System.out.print("index " + i + num[i]);
+		}
+		System.out.println();
+	}
+	
+}
